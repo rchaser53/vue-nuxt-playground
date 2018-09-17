41c4abaa1eebb579339acd5df15f2965f372ee29
rchaser53 <tayoshizawa29@gmail.com>
2018-09-17 14:30:40 +0900

    try to use errorCaptured
    but cannot catch click method
    only lifecycle?

---
+++ components/AppLogo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div @click="throwError">
+    nya-n
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('mounted child')
+    // a
+    throw new Error('test mounted error2')
+  },
+  methods: {
+    throwError() {
+      // a
+      // throw new Error('hoge-')
+    }
+  },
+  // errorCaptured(err, vm, info) {
+  //   console.log(err, "kokonihakuru");
+  //   return false;
+  // }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<script>
+import Vue from 'vue';
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
+
+Raven
+    .config('', {
+      shouldSendCallback() {
+        console.log('shouldSendCallback')
+        return true;
+      }
+    })
+    .addPlugin(RavenVue, Vue)
+    .install();
+
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  },
+  errorCaptured(err, vm, info) {
+    console.log(err, "errorCaptured!");
+    return false;
+  },
+  render (_) {
+    return <section>
+    <div>
+      test dayo
+      <app-logo/>
+    </div>
+  </section>
+  }
+}
+</script>
